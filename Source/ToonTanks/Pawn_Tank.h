b407bf0437b580a64d96b7c023c849705097e1ec
Mohamed Shaarawy <mhshaarawy3@outlook.com>
2020-10-12 15:37:48 +0200

    The starting point of the project where the Base pawn and level are done

---
+++ Source/ToonTanks/Pawn_Tank.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PawnBase.h"
+#include "Pawn_Tank.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API APawn_Tank : public APawnBase
+{
+	GENERATED_BODY()
+
+protected:
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	APawn_Tank();
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* Camera;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* SpringArm;
+
+	FVector MoveDirection;
+	FQuat RotationDirection;
+
+	void CalculateMoveInput(float Value);
+	void CalculateRotationInput(float Value);
+
+	void Move();
+	void Rotate();
+
+	float MoveSpeed = 100.0f;
+	float RotateSpeed = 100.0f;
+};
